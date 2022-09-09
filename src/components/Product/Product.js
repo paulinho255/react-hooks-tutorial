7bdc95986e9f148385d12803cd020e7d2898ceae
paulinho255 <paulinho255@gmail.com>
2022-09-09 17:37:05 -0300

    Adicionando função ao botão remove.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,21 +6,42 @@ const currencyOption = {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 };
-function cartReducer(state, product) {
-    return [...state, product]
+function cartReducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return [...state, action.nome];
+    case "remove":
+      const update = [...state];
+      update.splice(update.indexOf(action.nome), 1);
+      return update;
+    default:
+      return state;
+  }
 }
-function totalReducer(state, price) {
-    return state + price
+
+function totalReducer(state, action) {
+  if (action.type === "add") {
+    return state + action.price;
+  }
+  return state - action.price;
 }
 
 export default function Product() {
-  const [cart, setCart] = useReducer(cartReducer,[])
-  const [total, setTotal] = useReducer(totalReducer,0);
+  const [cart, setCart] = useReducer(cartReducer, []);
+  const [total, setTotal] = useReducer(totalReducer, 0);
 
   function add(product) {
-    setCart(product.nome);
-    setTotal(product.price);
+    const { nome, price } = product;
+    setCart({ nome, type: "add" });
+    setTotal({ price, type: "add" });
   }
+
+  function remove(product) {
+    const { nome, price } = product;
+    setCart({ nome, type: "remove" });
+    setTotal({ price, type: "remove" });
+  }
+
   function getTotal() {
     return total.toLocaleString(undefined, currencyOption);
   }
@@ -31,23 +52,22 @@ export default function Product() {
       <div>Total: {getTotal(total)}</div>
 
       <div>
-        {products.map(product => (
-            <div key={product.nome}>
-        <div className="product">
-          <span role="img" aria-label={product.nome}>
-             {product.emoji}            
-          </span>
-        </div>
-        <button onClick={() => add(product)}>Add</button>
-        <button
-          onClick={() => {
-            setCart([]);
-            setTotal(0);
-          }}
-        >
-          Remove
-        </button>
+        {products.map((product) => (
+          <div key={product.nome}>
+            <div className="product">
+              <span role="img" aria-label={product.nome}>
+                {product.emoji}
+              </span>
             </div>
+            <button onClick={() => add(product)}>Add</button>
+            <button
+              onClick={() => {
+                remove(product)
+              }}
+            >
+              Remove
+            </button>
+          </div>
         ))}
       </div>
     </div>
